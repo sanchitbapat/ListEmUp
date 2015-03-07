f0b002b58ea97b576cc9c4dedf404fd45c9eaeb4
sanchitbapat <sanchitbapat@gmail.com>
2015-03-07 03:30:10 -0700

    Database connection

--- app/src/main/java/com/listemup/listemup/ListActivity.java
+++ app/src/main/java/com/listemup/listemup/ListActivity.java
@@ -1,40 +1,16 @@
 package com.listemup.listemup;
 
-import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-
-import com.parse.Parse;
-import com.parse.ParseObject;
-
-
-public class MainActivity extends ActionBarActivity {
+public class ListActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        // Enable Local Datastore.
-        Parse.enableLocalDatastore(this);
-
-        Parse.initialize(this, "DCyuMQSuWLtitA7ABvpzP5rThP1c3RgmSePXuBYz", "JBBJl68IDFMk9pNXLbn8g0Ov8HLWBwDbgIlxxgV6");
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
-        Button list=(Button)findViewById(R.id.button);
-        list.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ListActivity.class));
-            }
-        });
-
-
+        setContentView(R.layout.activity_list);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
